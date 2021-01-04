783008e63cfc5780c8c01a9491f5a21705a9ea61
Shaletanu <shaletanu0@gmail.com>
2021-01-04 18:22:42 +0530

    Initial commit

---
+++ code_challenge_1.py
@@ -0,0 +1,6 @@
+#program to find simple interest
+p = float(input("Enter the principal amount in rupees: "))
+n = float(input("Enter the time duration in years: "))
+r = float(input("Enter the interest rate in percentage: "))
+simple_interest = (p*n*r)/100
+print("Simple Interest is", simple_interest)
